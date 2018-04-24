3c5fab849c9c297e43d9e02dfffa93f0d84a987b
Isaac Fiore <isaac.fiore@mail.mcgill.ca>
2018-04-24 20:17:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,8 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if (string === string.toUpperCase)
+  if (string === string.toUpperCase) {
+    return "YES INDEED!"
+  }
+  
 }
